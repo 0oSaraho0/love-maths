0b949d9e0e8bf232fa5acee0ae5ccc6cb8f7bed6
0oSaraho0 <0osarahtoddo0@gmail.com>
2022-01-21 15:33:33 +0000

    add code to create random numbers for the game question

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,9 +16,14 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 });
 
+/**The main game loop, called when the script is first loaded
+ * and after the user's awnser  been processed 
+ */
 
 function runGame() {
-
+    //creates random numbers between 1 and 25
+    let num1 = Math.floor(Math.random()*25)+1;
+    let num1 = Math.floor(Math.random()*25)+1;
 }
 
 function checkAnswer() {
